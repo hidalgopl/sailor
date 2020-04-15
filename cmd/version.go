38496529298ecfb6c9670d9398a81fee383e0c3f
hidalgopl <pawebojanowski@gmail.com>
2020-04-15 21:47:52 +0200

    bump version

--- cmd/version.go
+++ cmd/version.go
@@ -14,6 +14,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Sailor v0.0.1")
+		fmt.Println("Sailor v0.0.3")
 	},
 }
